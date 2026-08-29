f37798b20e872f04a6f8de57f2eb848216b9a2c6
Vladislav44-test <elertvelert@gmail.com>
2026-08-29 11:40:18 +0300

    Змінив код програмної частини для використання нової версії класу SwordDamage

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,48 +22,51 @@ namespace SwordDamageWPFTester
     public partial class MainWindow : Window
     {
         Random random = new Random();
-        SwordDamage swordDamage = new SwordDamage();
+        SwordDamage swordDamage;
 
         public MainWindow()
         {
+            swordDamage = new SwordDamage(random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7));
             InitializeComponent();
-            swordDamage.SetMagic(false);
-            swordDamage.SetFlaming(false);
+            swordDamage.Magic = false;
+            swordDamage.Flaming = false;
             RollDice();
         }
 
         private void RollDice()
         {
             swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            swordDamage.Flaming = flaming.IsChecked.Value;
+            swordDamage.Magic = magic.IsChecked.Value;
             DisplayDamage();
         }
 
         private void DisplayDamage()
         {
-            damage.Text = "Rolled " + swordDamage.Roll + " for " + swordDamage.Damage + " HP";
+            damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
         }
 
         private void flaming_Checked(object sender, RoutedEventArgs e)
         {
-            swordDamage.SetFlaming(true);
+            swordDamage.Flaming = true;
             DisplayDamage();
         }
 
         private void flaming_Unchecked(object sender, RoutedEventArgs e)
         {
-            swordDamage.SetFlaming(false);
+            swordDamage.Flaming = false;
             DisplayDamage();
         }
 
         private void magic_Checked(object sender, RoutedEventArgs e)
         {
-            swordDamage.SetMagic(true);
+            swordDamage.Magic = true;
             DisplayDamage();
         }
 
         private void magic_Unchecked(object sender, RoutedEventArgs e)
         {
-            swordDamage.SetMagic(false);
+            swordDamage.Magic = false;
             DisplayDamage();
         }
 
--- SwordDamage.cs
+++ SwordDamage.cs
@@ -8,36 +8,79 @@ namespace SwordDamageTester
 {
     internal class SwordDamage
     {
-        public const int BASE_DAMAGE = 3;
-        public const int FLAME_DAMAGE = 2;
-        public int Roll;
-        public decimal MagicMultiplier = 1M;
-        public int FlamingDamage = 0;
-        public int Damage;
+        /// <summary>
+        /// Константа для додавання до шкоди під час її вирахування (базова шкода).
+        /// </summary>
+        private const int BASE_DAMAGE = 3;
+        /// <summary>
+        /// Константа для додавання до шкоди під час її вирахування (додаткова шкода від вогняного ефекту).
+        /// </summary>
+        private const int FLAME_DAMAGE = 2;
 
-        public void CalculateDamage()
+        private int roll;
+        /// <summary>
+        /// Видає або задає результат кидку 3D6.
+        /// </summary>
+        public int Roll
         {
-            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
         }
-        public void SetMagic(bool isMagic)
+        private bool flaming;
+        /// <summary>
+        /// Видає або задає вогняний ефект для меча.
+        /// </summary>
+        public bool Flaming
         {
-            if (isMagic)
+            get { return flaming; }
+            set
             {
-                MagicMultiplier = 1.75M;
+                flaming = value;
+                CalculateDamage();
             }
-            else
-            {
-                MagicMultiplier = 1M;
-            }
-            CalculateDamage();
         }
-        public void SetFlaming(bool isFlaming)
+        private bool magic;
+        /// <summary>
+        /// Видає або задає магічний ефект для меча.
+        /// </summary>
+        public bool Magic
         {
-            CalculateDamage();
-            if (isFlaming)
+            get { return magic; }
+            set
             {
-                Damage += FLAME_DAMAGE;
+                magic = value;
+                CalculateDamage();
             }
+        }
+        /// <summary>
+        /// Видає або задає значення вирахуваної шкоди від меча.
+        /// </summary>
+        public int Damage { get; private set; }
+
+        /// <summary>
+        /// Вираховує значення шкоди по формулі, використовуючи значення Roll, Magic та Flaming
+        /// (Якщо меч магічний, результат кидку множиться на 1.75; якщо меч вогняний, до вирахуваної шкоди додаються пошкодження від вогню).
+        /// </summary>
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+
+        /// <summary>
+        /// Задає початкове значення кидка 3D6 властивості Roll та вираховує початкову шкоду від меча.
+        /// </summary>
+        /// <param name="roll">Значення кидка 3D6.</param>
+        public SwordDamage(int roll)
+        {
+            Roll = roll;
+            CalculateDamage();
         }
     }
 }
